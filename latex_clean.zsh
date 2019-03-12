2ebe020c81a545f4dacd38fb44fee2206756e68e
Devon Morris <devonmorris1992@gmail.com>
2019-03-12 09:41:12 -0600

    clean up

--- latex_clean.zsh
+++ latex_clean.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 setopt shwordsplit
-exts="aux bbl blg brf idx ilg ind lof log lol lot out toc synctex.gz fls fdb_latexmk"
+exts="aux bbl blg brf idx ilg ind lof log lol lot out toc synctex.gz fls fdb_latexmk pdf"
 
 for ext in $exts; do
      rm -f **/*.$ext
